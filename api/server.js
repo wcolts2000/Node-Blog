aeb1985ded00f7034dea5fdef13e06c6e11ef8ff
wcolts2000 <wcolts2000@gmail.com>
2019-01-09 16:10:51 -0800

    added returns to middleware function to remove cant add header after they are sent error

--- api/server.js
+++ api/server.js
@@ -21,14 +21,14 @@ function uppercaseUsername(req, res, next) {
   let { name } = req.body;
 
   if (!name) {
-    res.status(400).json({ message: "Please provide a UserName, thank you" });
+    return res
+      .status(400)
+      .json({ message: "Please provide a UserName, thank you" });
   }
   if (name.length > 128) {
-    res
-      .status(400)
-      .json({
-        message: "Username can not exceed 128 characters, i mean, come on!!!"
-      });
+    return res.status(400).json({
+      message: "Username can not exceed 128 characters, i mean, come on!!!"
+    });
   }
   if (name[0] !== name[0].toUpperCase()) {
     res.status(400).json({
